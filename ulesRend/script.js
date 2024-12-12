9d4d619d31edf1fbbcec5fcc2ed04465fb3aacec
Keszericze Ákos <keszericzea21@ady-nagyatad.sulinet.hu>
2024-12-12 12:32:33 +0100

    asd

---
+++ ulesRend/script.js
@@ -0,0 +1,8 @@
+let canvas = document.getElementById("aula");
+let taratlomDim = canvas.getContext("2d");
+
+canvas.width = window.innerWidth/2;
+canvas.height = window.innerHeight-15;
+
+canvas.style.border = "5px solid red";
+canvas.style.
