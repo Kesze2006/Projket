56891cd0300b3bdf72434860d4c0db39a7639abe
Kesze2006 <keszericze.akos.21@ady-nagyatad.hu>
2025-01-06 22:33:46 +0100

    asd

--- ulesRend/script.js
+++ ulesRend/script.js
@@ -1,8 +1,22 @@
 let canvas = document.getElementById("aula");
-let taratlomDim = canvas.getContext("2d");
+let tartalom = canvas.getContext("2d");
 
-canvas.width = window.innerWidth/2;
-canvas.height = window.innerHeight-15;
+canvas.width = window.innerWidth / 2;
+canvas.height = window.innerHeight - 15;
 
-canvas.style.border = "5px solid red";
-canvas.style.
+let targyak = []
+
+class targy {
+  constructor(x, y, width, height, tipus) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.tipus = tipus;
+  }
+}
+
+megRajzol()
+{
+
+}
