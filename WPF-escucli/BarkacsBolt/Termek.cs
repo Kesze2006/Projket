46d0c61af1ff954d2c292f4e7e248bc447823f93
Keszericze Ákos <keszericzea21@ady-nagyatad.sulinet.hu>
2025-04-11 09:40:38 +0200

    asd

---
+++ WPF-escucli/BarkacsBolt/Termek.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarkacsBolt
+{
+    internal class Termek
+    {
+        public int Id { get; set; }
+        public string Nev { get; set; }
+        public string Kategoria { get; set; }
+        public int Keszlet { get; set; }
+        public decimal Ar { get; set; }
+    }
+}
