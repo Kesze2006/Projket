b54cc70bb90914ff25618b534ca18af254dfb08a
Keszericze Ákos <keszericzea21@ady-nagyatad.sulinet.hu>
2025-05-13 10:36:10 +0200

    asd

---
+++ WPF-escucli/bolt_terv/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace bolt_terv
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
